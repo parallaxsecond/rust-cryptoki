fce630896b5e1db7c4337d120a114b197af3f56b
Benno Rice <benno@jeamland.net>
2021-07-02 17:08:34 +1000

    Add x86_64 macOS/Darwin bindings.
    Signed-off-by: Benno Rice <benno@jeamland.net>

--- cryptoki-sys/build.rs
+++ cryptoki-sys/build.rs
@@ -20,6 +20,7 @@ fn main() {
             (Architecture::X86_32(_), OperatingSystem::Linux) => {}
             (Architecture::Powerpc64, OperatingSystem::Linux) => {}
             (Architecture::Powerpc64le, OperatingSystem::Linux) => {}
+            (Architecture::X86_64, OperatingSystem::Darwin) => {}
             (arch, os) => {
                 panic!("Compilation target (architecture, OS) tuple ({}, {}) is not part of the supported tuples. Please compile with the \"generate-bindings\" feature or add support for your platform :)", arch, os);
             }
