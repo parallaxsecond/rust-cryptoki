05413d0b1f6f449d6245a9291b758fa105b90bf7
Keith Koskie <vkkoskie@gmail.com>
2021-11-11 11:17:18 -0500

    Refactor TokenInfo Flags
    Signed-off-by: Keith Koskie <vkkoskie@gmail.com>

--- cryptoki/src/slot/flags.rs
+++ cryptoki/src/slot/flags.rs
@@ -2,10 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 //! PKCS11 General Data Types
 
-use super::SlotInfo;
-use crate::types::Flags;
+use super::{SlotInfo, TokenInfo};
 use cryptoki_sys::*;
-use std::fmt::Formatter;
+use std::fmt::{self, Debug, Display, Formatter};
 
 use crate::flag::*;
 
@@ -14,8 +13,8 @@ pub(crate) const TOKEN_PRESENT: FlagBit<SlotInfo> = FlagBit::new(CKF_TOKEN_PRESE
 pub(crate) const REMOVABLE_DEVICE: FlagBit<SlotInfo> = FlagBit::new(CKF_REMOVABLE_DEVICE);
 pub(crate) const HW_SLOT: FlagBit<SlotInfo> = FlagBit::new(CKF_HW_SLOT);
 
-impl core::fmt::Debug for CkFlags<SlotInfo> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+impl Debug for CkFlags<SlotInfo> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.debug_struct("Flags")
             .field("token_present", &(self.contains(TOKEN_PRESENT)))
             .field("removable_device", &(self.contains(REMOVABLE_DEVICE)))
@@ -24,8 +23,8 @@ impl core::fmt::Debug for CkFlags<SlotInfo> {
     }
 }
 
-impl core::fmt::Display for CkFlags<SlotInfo> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+impl Display for CkFlags<SlotInfo> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let mut set = f.debug_set();
         if self.contains(TOKEN_PRESENT) {
             let _ = set.entry(&"Token Present");
@@ -40,295 +39,138 @@ impl core::fmt::Display for CkFlags<SlotInfo> {
     }
 }
 
-#[derive(Debug, Default, Clone, Copy)]
-/// Collection of flags defined for [`CK_TOKEN_INFO`]
-pub struct TokenFlags {
-    flags: CK_FLAGS,
-}
+pub(crate) const RNG: FlagBit<TokenInfo> = FlagBit::new(CKF_RNG);
+pub(crate) const WRITE_PROTECTED: FlagBit<TokenInfo> = FlagBit::new(CKF_WRITE_PROTECTED);
+pub(crate) const LOGIN_REQUIRED: FlagBit<TokenInfo> = FlagBit::new(CKF_LOGIN_REQUIRED);
+pub(crate) const USER_PIN_INITIALIZED: FlagBit<TokenInfo> = FlagBit::new(CKF_USER_PIN_INITIALIZED);
+pub(crate) const RESTORE_KEY_NOT_NEEDED: FlagBit<TokenInfo> =
+    FlagBit::new(CKF_RESTORE_KEY_NOT_NEEDED);
+pub(crate) const CLOCK_ON_TOKEN: FlagBit<TokenInfo> = FlagBit::new(CKF_CLOCK_ON_TOKEN);
+pub(crate) const PROTECTED_AUTHENTICATION_PATH: FlagBit<TokenInfo> =
+    FlagBit::new(CKF_PROTECTED_AUTHENTICATION_PATH);
+pub(crate) const DUAL_CRYPTO_OPERATIONS: FlagBit<TokenInfo> =
+    FlagBit::new(CKF_DUAL_CRYPTO_OPERATIONS);
+pub(crate) const TOKEN_INITIALIZED: FlagBit<TokenInfo> = FlagBit::new(CKF_TOKEN_INITIALIZED);
+pub(crate) const SECONDARY_AUTHENTICATION: FlagBit<TokenInfo> =
+    FlagBit::new(CKF_SECONDARY_AUTHENTICATION);
+pub(crate) const USER_PIN_COUNT_LOW: FlagBit<TokenInfo> = FlagBit::new(CKF_USER_PIN_COUNT_LOW);
+pub(crate) const USER_PIN_FINAL_TRY: FlagBit<TokenInfo> = FlagBit::new(CKF_USER_PIN_FINAL_TRY);
+pub(crate) const USER_PIN_LOCKED: FlagBit<TokenInfo> = FlagBit::new(CKF_USER_PIN_LOCKED);
+pub(crate) const USER_PIN_TO_BE_CHANGED: FlagBit<TokenInfo> =
+    FlagBit::new(CKF_USER_PIN_TO_BE_CHANGED);
+pub(crate) const SO_PIN_COUNT_LOW: FlagBit<TokenInfo> = FlagBit::new(CKF_SO_PIN_COUNT_LOW);
+pub(crate) const SO_PIN_FINAL_TRY: FlagBit<TokenInfo> = FlagBit::new(CKF_SO_PIN_FINAL_TRY);
+pub(crate) const SO_PIN_LOCKED: FlagBit<TokenInfo> = FlagBit::new(CKF_SO_PIN_LOCKED);
+pub(crate) const SO_PIN_TO_BE_CHANGED: FlagBit<TokenInfo> = FlagBit::new(CKF_SO_PIN_TO_BE_CHANGED);
+pub(crate) const ERROR_STATE: FlagBit<TokenInfo> = FlagBit::new(CKF_ERROR_STATE);
 
-impl Flags for TokenFlags {
-    type FlagType = CK_FLAGS;
-
-    fn flag_value(&self) -> Self::FlagType {
-        self.flags
-    }
-
-    fn flag(&self, flag: Self::FlagType) -> bool {
-        self.flag_value() & flag == flag
-    }
-
-    fn set_flag(&mut self, flag: Self::FlagType, b: bool) {
-        if b {
-            self.flags |= flag;
-        } else {
-            self.flags &= !flag;
-        }
-    }
-
-    fn stringify_flag(flag: Self::FlagType) -> &'static str {
-        match flag {
-            CKF_RNG => std::stringify!(CKF_RNG),
-            CKF_WRITE_PROTECTED => std::stringify!(CKF_WRITE_PROTECTED),
-            CKF_LOGIN_REQUIRED => std::stringify!(CKF_LOGIN_REQUIRED),
-            CKF_USER_PIN_INITIALIZED => std::stringify!(CKF_USER_PIN_INITIALIZED),
-            CKF_RESTORE_KEY_NOT_NEEDED => std::stringify!(CKF_RESTORE_KEY_NOT_NEEDED),
-            CKF_CLOCK_ON_TOKEN => std::stringify!(CKF_CLOCK_ON_TOKEN),
-            CKF_PROTECTED_AUTHENTICATION_PATH => std::stringify!(CKF_PROTECTED_AUTHENTICATION_PATH),
-            CKF_DUAL_CRYPTO_OPERATIONS => std::stringify!(CKF_DUAL_CRYPTO_OPERATIONS),
-            CKF_TOKEN_INITIALIZED => std::stringify!(CKF_TOKEN_INITIALIZED),
-            CKF_SECONDARY_AUTHENTICATION => std::stringify!(CKF_SECONDARY_AUTHENTICATION),
-            CKF_USER_PIN_COUNT_LOW => std::stringify!(CKF_USER_PIN_COUNT_LOW),
-            CKF_USER_PIN_FINAL_TRY => std::stringify!(CKF_USER_PIN_FINAL_TRY),
-            CKF_USER_PIN_LOCKED => std::stringify!(CKF_USER_PIN_LOCKED),
-            CKF_USER_PIN_TO_BE_CHANGED => std::stringify!(CKF_USER_PIN_TO_BE_CHANGED),
-            CKF_SO_PIN_COUNT_LOW => std::stringify!(CKF_SO_PIN_COUNT_LOW),
-            CKF_SO_PIN_FINAL_TRY => std::stringify!(CKF_SO_PIN_FINAL_TRY),
-            CKF_SO_PIN_LOCKED => std::stringify!(CKF_SO_PIN_LOCKED),
-            CKF_SO_PIN_TO_BE_CHANGED => std::stringify!(CKF_SO_PIN_TO_BE_CHANGED),
-            _ => "Unknown CK_TOKEN_INFO flag",
-        }
-    }
-}
-
-impl TokenFlags {
-    /// Creates a new instance of `TokenFlags` with no flags set
-    pub fn new() -> Self {
-        TokenFlags::default()
-    }
-
-    /// Gets value of [`CKF_RNG`]
-    pub fn random_number_generator(&self) -> bool {
-        self.flag(CKF_RNG)
-    }
-
-    /// Sets value of [`CKF_RNG`]
-    pub fn set_random_number_generator(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_RNG, b);
-        self
-    }
-
-    /// Gets value of [`CKF_WRITE_PROTECTED`]
-    pub fn write_protected(&self) -> bool {
-        self.flag(CKF_WRITE_PROTECTED)
-    }
-
-    /// Sets value of [`CKF_WRITE_PROTECTED`]
-    pub fn set_write_protected(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_WRITE_PROTECTED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_LOGIN_REQUIRED`]
-    pub fn login_required(&self) -> bool {
-        self.flag(CKF_LOGIN_REQUIRED)
-    }
-
-    /// Sets value of [`CKF_LOGIN_REQUIRED`]
-    pub fn set_login_required(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_LOGIN_REQUIRED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_USER_PIN_INITIALIZED`]
-    pub fn user_pin_initialized(&self) -> bool {
-        self.flag(CKF_USER_PIN_INITIALIZED)
-    }
-
-    /// Sets value of [`CKF_USER_PIN_INITIALIZED`]
-    pub fn set_user_pin_initialized(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_USER_PIN_INITIALIZED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_RESTORE_KEY_NOT_NEEDED`]
-    pub fn restore_key_not_needed(&self) -> bool {
-        self.flag(CKF_RESTORE_KEY_NOT_NEEDED)
-    }
-
-    /// Sets value of [`CKF_RESTORE_KEY_NOT_NEEDED`]
-    pub fn set_restore_key_not_needed(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_RESTORE_KEY_NOT_NEEDED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_CLOCK_ON_TOKEN`]
-    pub fn clock_on_token(&self) -> bool {
-        self.flag(CKF_CLOCK_ON_TOKEN)
-    }
-
-    /// Sets value of [`CKF_CLOCK_ON_TOKEN`]
-    pub fn set_clock_on_token(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_CLOCK_ON_TOKEN, b);
-        self
-    }
-
-    /// Gets value of [`CKF_PROTECTED_AUTHENTICATION_PATH`]
-    pub fn protected_authentication_path(&self) -> bool {
-        self.flag(CKF_PROTECTED_AUTHENTICATION_PATH)
-    }
-
-    /// Sets value of [`CKF_PROTECTED_AUTHENTICATION_PATH`]
-    pub fn set_protected_authentication_path(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_PROTECTED_AUTHENTICATION_PATH, b);
-        self
-    }
-
-    /// Gets value of [`CKF_DUAL_CRYPTO_OPERATIONS`]
-    pub fn dual_crypto_operations(&self) -> bool {
-        self.flag(CKF_DUAL_CRYPTO_OPERATIONS)
-    }
-
-    /// Sets value of [`CKF_DUAL_CRYPTO_OPERATIONS`]
-    pub fn set_dual_crypto_operations(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_DUAL_CRYPTO_OPERATIONS, b);
-        self
-    }
-
-    /// Gets value of [`CKF_TOKEN_INITIALIZED`]
-    pub fn token_initialized(&self) -> bool {
-        self.flag(CKF_TOKEN_INITIALIZED)
-    }
-
-    /// Sets value of [`CKF_TOKEN_INITIALIZED`]
-    pub fn set_token_initialized(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_TOKEN_INITIALIZED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_SECONDARY_AUTHENTICATION`]
-    pub fn secondary_authentication(&self) -> bool {
-        self.flag(CKF_SECONDARY_AUTHENTICATION)
-    }
-
-    /// Sets value of [`CKF_SECONDARY_AUTHENTICATION`]
-    pub fn set_secondary_authentication(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_SECONDARY_AUTHENTICATION, b);
-        self
-    }
-
-    /// Gets value of [`CKF_USER_PIN_COUNT_LOW`]
-    pub fn user_pin_count_low(&self) -> bool {
-        self.flag(CKF_USER_PIN_COUNT_LOW)
-    }
-
-    /// Sets value of [`CKF_USER_PIN_COUNT_LOW`]
-    pub fn set_user_pin_count_low(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_USER_PIN_COUNT_LOW, b);
-        self
-    }
-
-    /// Gets value of [`CKF_USER_PIN_FINAL_TRY`]
-    pub fn user_pin_final_try(&self) -> bool {
-        self.flag(CKF_USER_PIN_FINAL_TRY)
-    }
-
-    /// Sets value of [`CKF_USER_PIN_FINAL_TRY`]
-    pub fn set_user_pin_final_try(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_USER_PIN_FINAL_TRY, b);
-        self
-    }
-
-    /// Gets value of [`CKF_USER_PIN_LOCKED`]
-    pub fn user_pin_locked(&self) -> bool {
-        self.flag(CKF_USER_PIN_LOCKED)
-    }
-
-    /// Sets value of [`CKF_USER_PIN_LOCKED`]
-    pub fn set_user_pin_locked(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_USER_PIN_LOCKED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_USER_PIN_TO_BE_CHANGED`]
-    pub fn user_pin_to_be_changed(&self) -> bool {
-        self.flag(CKF_USER_PIN_TO_BE_CHANGED)
-    }
-
-    /// Sets value of [`CKF_USER_PIN_TO_BE_CHANGED`]
-    pub fn set_user_pin_to_be_changed(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_USER_PIN_TO_BE_CHANGED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_SO_PIN_COUNT_LOW`]
-    pub fn so_pin_count_low(&self) -> bool {
-        self.flag(CKF_SO_PIN_COUNT_LOW)
-    }
-
-    /// Sets value of [`CKF_SO_PIN_COUNT_LOW`]
-    pub fn set_so_pin_count_low(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_SO_PIN_COUNT_LOW, b);
-        self
-    }
-
-    /// Gets value of [`CKF_SO_PIN_FINAL_TRY`]
-    pub fn so_pin_final_try(&self) -> bool {
-        self.flag(CKF_SO_PIN_FINAL_TRY)
-    }
-
-    /// Sets value of [`CKF_SO_PIN_FINAL_TRY`]
-    pub fn set_so_pin_final_try(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_SO_PIN_FINAL_TRY, b);
-        self
-    }
-
-    /// Gets value of [`CKF_SO_PIN_LOCKED`]
-    pub fn so_pin_locked(&self) -> bool {
-        self.flag(CKF_SO_PIN_LOCKED)
-    }
-
-    /// Sets value of [`CKF_SO_PIN_LOCKED`]
-    pub fn set_so_pin_locked(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_SO_PIN_LOCKED, b);
-        self
-    }
-
-    /// Gets value of [`CKF_SO_PIN_TO_BE_CHANGED`]
-    pub fn so_pin_to_be_changed(&self) -> bool {
-        self.flag(CKF_SO_PIN_TO_BE_CHANGED)
-    }
-
-    /// Sets value of [`CKF_SO_PIN_TO_BE_CHANGED`]
-    pub fn set_so_pin_to_be_changed(&mut self, b: bool) -> &mut Self {
-        self.set_flag(CKF_SO_PIN_TO_BE_CHANGED, b);
-        self
-    }
-}
-
-impl std::fmt::Display for TokenFlags {
+impl Debug for CkFlags<TokenInfo> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let flags = vec![
-            CKF_RNG,
-            CKF_WRITE_PROTECTED,
-            CKF_LOGIN_REQUIRED,
-            CKF_USER_PIN_INITIALIZED,
-            CKF_RESTORE_KEY_NOT_NEEDED,
-            CKF_CLOCK_ON_TOKEN,
-            CKF_PROTECTED_AUTHENTICATION_PATH,
-            CKF_DUAL_CRYPTO_OPERATIONS,
-            CKF_TOKEN_INITIALIZED,
-            CKF_SECONDARY_AUTHENTICATION,
-            CKF_USER_PIN_COUNT_LOW,
-            CKF_USER_PIN_FINAL_TRY,
-            CKF_USER_PIN_LOCKED,
-            CKF_USER_PIN_TO_BE_CHANGED,
-            CKF_SO_PIN_COUNT_LOW,
-            CKF_SO_PIN_FINAL_TRY,
-            CKF_SO_PIN_LOCKED,
-            CKF_SO_PIN_TO_BE_CHANGED,
-        ];
-        self.stringify_fmt(f, flags)
+        f.debug_struct("Flags")
+            .field("rng", &(self.contains(RNG)))
+            .field("write_protected", &(self.contains(WRITE_PROTECTED)))
+            .field("login_required", &(self.contains(LOGIN_REQUIRED)))
+            .field(
+                "user_pin_initialized",
+                &(self.contains(USER_PIN_INITIALIZED)),
+            )
+            .field(
+                "restore_key_not_needed",
+                &(self.contains(RESTORE_KEY_NOT_NEEDED)),
+            )
+            .field("clock_on token", &(self.contains(CLOCK_ON_TOKEN)))
+            .field(
+                "protected_authentication_path",
+                &(self.contains(PROTECTED_AUTHENTICATION_PATH)),
+            )
+            .field(
+                "dual_crypto_operations",
+                &(self.contains(DUAL_CRYPTO_OPERATIONS)),
+            )
+            .field("token_initialized", &(self.contains(TOKEN_INITIALIZED)))
+            .field(
+                "secondary_authentication",
+                &(self.contains(SECONDARY_AUTHENTICATION)),
+            )
+            .field("user_pin_count_low", &(self.contains(USER_PIN_COUNT_LOW)))
+            .field("user_pin_final_try", &(self.contains(USER_PIN_FINAL_TRY)))
+            .field("user_pin_locked", &(self.contains(USER_PIN_LOCKED)))
+            .field(
+                "user_pin_to_be_changed",
+                &(self.contains(USER_PIN_TO_BE_CHANGED)),
+            )
+            .field("so_pin_count_low", &(self.contains(SO_PIN_COUNT_LOW)))
+            .field("so_pin_final_try", &(self.contains(SO_PIN_FINAL_TRY)))
+            .field("so_pin_locked", &(self.contains(SO_PIN_LOCKED)))
+            .field(
+                "so_pin_to_be_changed",
+                &(self.contains(SO_PIN_TO_BE_CHANGED)),
+            )
+            .field("error_state", &(self.contains(ERROR_STATE)))
+            .finish()
     }
 }
 
-impl From<TokenFlags> for CK_FLAGS {
-    fn from(flags: TokenFlags) -> Self {
-        flags.flags
-    }
-}
-
-impl From<CK_FLAGS> for TokenFlags {
-    fn from(flags: CK_FLAGS) -> Self {
-        Self { flags }
+impl Display for CkFlags<TokenInfo> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut set = f.debug_set();
+        if self.contains(RNG) {
+            let _ = set.entry(&"Random Number Generator");
+        }
+        if self.contains(WRITE_PROTECTED) {
+            let _ = set.entry(&"Write-Protected");
+        }
+        if self.contains(LOGIN_REQUIRED) {
+            let _ = set.entry(&"Login Required");
+        }
+        if self.contains(USER_PIN_INITIALIZED) {
+            let _ = set.entry(&"User Pin Initialized");
+        }
+        if self.contains(RESTORE_KEY_NOT_NEEDED) {
+            let _ = set.entry(&"Restore Key Not Needed");
+        }
+        if self.contains(CLOCK_ON_TOKEN) {
+            let _ = set.entry(&"Clock on Token");
+        }
+        if self.contains(PROTECTED_AUTHENTICATION_PATH) {
+            let _ = set.entry(&"Protected Authentication Path");
+        }
+        if self.contains(DUAL_CRYPTO_OPERATIONS) {
+            let _ = set.entry(&"Dual Crypto Operations");
+        }
+        if self.contains(TOKEN_INITIALIZED) {
+            let _ = set.entry(&"Token Initialized");
+        }
+        if self.contains(SECONDARY_AUTHENTICATION) {
+            let _ = set.entry(&"Secondary Authentication");
+        }
+        if self.contains(USER_PIN_COUNT_LOW) {
+            let _ = set.entry(&"User PIN Count Low");
+        }
+        if self.contains(USER_PIN_FINAL_TRY) {
+            let _ = set.entry(&"User PIN Final Try");
+        }
+        if self.contains(USER_PIN_LOCKED) {
+            let _ = set.entry(&"User PIN Locked");
+        }
+        if self.contains(USER_PIN_TO_BE_CHANGED) {
+            let _ = set.entry(&"User PIN to be Changed");
+        }
+        if self.contains(SO_PIN_COUNT_LOW) {
+            let _ = set.entry(&"Security Officer PIN Count Low");
+        }
+        if self.contains(SO_PIN_FINAL_TRY) {
+            let _ = set.entry(&"Security Officer PIN Final Try");
+        }
+        if self.contains(SO_PIN_LOCKED) {
+            let _ = set.entry(&"Security Officer PIN Locked");
+        }
+        if self.contains(SO_PIN_TO_BE_CHANGED) {
+            let _ = set.entry(&"Security Officer PIN to be Changed");
+        }
+        if self.contains(ERROR_STATE) {
+            let _ = set.entry(&"Error State");
+        }
+        set.finish()
     }
 }
