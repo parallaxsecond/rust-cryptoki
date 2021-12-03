67e758928631c68f3ed4db601df8e58dd95ad50a
Keith Koskie <vkkoskie@gmail.com>
2021-12-03 18:59:59 -0500

    Replace SessionFlags with bool to open session
    There are only two flags supported as arguments when opening a
    session.
    
    One of them must always be true, but perversely defaults to false.
    This forces client code to construct a trivial value to pass. This
    commit now hides this flag, setting it to its only valid value always.
    This also removes a single test which checked for failure when the flag
    was set to false.
    
    With only one flag (read/write) remaining, the open session call now
    accepts a boolean for the option and conversion to a wider integer
    type is handled internally.
    
    Signed-off-by: Keith Koskie <vkkoskie@gmail.com>

--- cryptoki/src/context/session_management.rs
+++ cryptoki/src/context/session_management.rs
@@ -2,25 +2,31 @@
 // SPDX-License-Identifier: Apache-2.0
 //! Session management functions
 
+use cryptoki_sys::{CKF_RW_SESSION, CKF_SERIAL_SESSION};
+
 use crate::context::Pkcs11;
 use crate::error::{Result, Rv};
-use crate::session::{Session, SessionFlags};
+use crate::session::Session;
 use crate::slot::Slot;
 use std::convert::TryInto;
-
 // See public docs on stub in parent mod.rs
 #[inline(always)]
 pub(super) fn open_session_no_callback(
     ctx: &Pkcs11,
     slot_id: Slot,
-    flags: SessionFlags,
+    read_write: bool,
 ) -> Result<Session> {
     let mut session_handle = 0;
 
+    let flags = if read_write {
+        CKF_SERIAL_SESSION | CKF_RW_SESSION
+    } else {
+        CKF_SERIAL_SESSION
+    };
     unsafe {
         Rv::from(get_pkcs11!(ctx, C_OpenSession)(
             slot_id.try_into()?,
-            flags.into(),
+            flags,
             // TODO: abstract those types or create new functions for callbacks
             std::ptr::null_mut(),
             None,
