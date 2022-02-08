59173d1904e8259c513542ffb43f55e6ac311ac0
Ionut Mihalcea <ionut.mihalcea@arm.com>
2022-02-08 12:54:17 +0000

    Add new bindings and regeneration script
    A new script is added to aid the regeneration of bindings whenever the
    bindgen version is changed, along with the bindings produced with
    bindgen v0.59.2.
    
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

---
+++ cryptoki-sys/regenerate_bindings.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Contributors to the Parsec project.
+# SPDX-License-Identifier: Apache-2.0
+
+set -xeuf -o pipefail
+
+# x86_64-unknown-darwin is not included as the stable toolchain does not
+# include `rust-std` as a components (see https://doc.rust-lang.org/nightly/rustc/platform-support.html)
+targets=( aarch64-unknown-linux-gnu arm-unknown-linux-gnueabi i686-unknown-linux-gnu powerpc64-unknown-linux-gnu x86_64-unknown-linux-gnu )
+
+grepcwe() { grep -c "$@" || test $? = 1; }
+
+for target in "${targets[@]}"
+do
+	PREVIOUSLY_INSTALLED=`rustup target list | grepcwe "$target (installed)"`
+
+    if [ "$PREVIOUSLY_INSTALLED" == "0" ]; then
+        rustup target install $target
+    fi
+
+    cargo build --target $target --features generate-bindings
+    find ../target/$target/ -name pkcs11_bindings.rs | xargs -I '{}' cp '{}' src/bindings/$target.rs
+
+    if [ "$PREVIOUSLY_INSTALLED" == "0" ]; then
+        rustup target remove $target
+    fi
+done
