e9a694e8e8f1c89de535c1ea05e1d16b30c407b3
Ionut Mihalcea <ionut.mihalcea@arm.com>
2022-05-03 13:40:39 +0100

    Add script for regenerating binding files
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

---
+++ cryptoki-sys/regenerate_bindings.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Contributors to the Parsec project.
+# SPDX-License-Identifier: Apache-2.0
+
+set -xeuf -o pipefail
+
+targets="aarch64-unknown-linux-gnu arm-unknown-linux-gnueabi i686-unknown-linux-gnu powerpc64-unknown-linux-gnu x86_64-unknown-linux-gnu x86_64-apple-darwin aarch64-apple-darwin"
+TARGET_INSTALLED=
+
+for target in $targets; do
+
+    # Check if the target is already installed
+    if ! rustup target list | grep -q "$target (installed)"; then
+        rustup target install $target
+        TARGET_INSTALLED="$target"
+    fi
+
+    cargo build --target $target --features generate-bindings
+    find ../target/$target/ -name pkcs11_bindings.rs | xargs -I '{}' cp '{}' src/bindings/$target.rs
+
+    if [ "$TARGET_INSTALLED" == "$target" ]; then
+        rustup target remove $target
+    fi
+done
