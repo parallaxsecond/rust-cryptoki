359afe0a91e5e3067016b7b2cfe27511b34fa825
daxpedda <daxpedda@gmail.com>
2025-04-22 11:58:46 +0200

    Test parallel access
    Signed-off-by: daxpedda <daxpedda@gmail.com>

---
+++ cryptoki/tests/parallel.rs
@@ -0,0 +1,13 @@
+// Copyright 2021 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+mod common;
+
+#[test]
+fn test1() {
+    common::init_pins();
+}
+
+#[test]
+fn test2() {
+    common::init_pins();
+}
