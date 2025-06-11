144fe24d92d8c6faef5f08f3d18d1392c97d91c4
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2025-06-11 08:57:47 +0200

    Build and test in a matrix
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- ci.sh
+++ ci.sh
@@ -5,10 +5,10 @@
 
 # Continuous Integration test script
 
-set -euf -o pipefail
+set -euxf -o pipefail
 
 pushd cryptoki-sys
-RUST_BACKTRACE=1 cargo build --features generate-bindings
+cargo build --features generate-bindings
 popd
 
 # check formatting before going through all the builds
@@ -19,22 +19,10 @@ if cargo clippy --version; then
 	cargo clippy --all-targets -- -D clippy::all -D clippy::cargo
 fi
 
-RUST_BACKTRACE=1 cargo build
+cargo build
 
-RUST_BACKTRACE=1 cargo build --all-features
+cargo build --all-features
 
-RUST_BACKTRACE=1 cargo build --target arm-unknown-linux-gnueabi
-RUST_BACKTRACE=1 cargo build --target armv7-unknown-linux-gnueabi
-RUST_BACKTRACE=1 cargo build --target armv7-unknown-linux-gnueabihf
-RUST_BACKTRACE=1 cargo build --target aarch64-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target i686-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target loongarch64-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target powerpc64-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target powerpc64le-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target riscv64gc-unknown-linux-gnu
-RUST_BACKTRACE=1 cargo build --target x86_64-pc-windows-msvc
-RUST_BACKTRACE=1 cargo build --target x86_64-apple-darwin
-RUST_BACKTRACE=1 cargo build --target aarch64-apple-darwin
-RUST_BACKTRACE=1 cargo build --target x86_64-unknown-freebsd
+cargo build --target "$TARGET"
 
-RUST_BACKTRACE=1 cargo test
+cargo test
