dcda207810508ae04cc4bf81a6840a3dde113a54
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2025-06-16 09:04:49 +0200

    Split tests from checks
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- ci.sh
+++ ci.sh
@@ -11,18 +11,4 @@ pushd cryptoki-sys
 cargo build --features generate-bindings
 popd
 
-# check formatting before going through all the builds
-if cargo fmt --version; then
-	cargo fmt --all -- --check
-fi
-if cargo clippy --version; then
-	cargo clippy --all-targets -- -D clippy::all -D clippy::cargo
-fi
-
-cargo build
-
-cargo build --all-features
-
-cargo build --target "$TARGET"
-
-cargo test
+cargo test --target "$TARGET"
