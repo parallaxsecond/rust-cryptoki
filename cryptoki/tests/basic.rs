dce5711195b241629174869f7791d0b6f13211f4
Jakub Jelen <jjelen@redhat.com>
2025-08-18 18:23:03 +0200

    tests: Be less strict about error type when doing SetAttribute
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>

--- cryptoki/tests/basic.rs
+++ cryptoki/tests/basic.rs
@@ -3823,10 +3823,7 @@ fn unique_id() -> TestResult {
     } else {
         assert!(matches!(
             res,
-            Err(Error::Pkcs11(
-                RvError::AttributeReadOnly,
-                Function::SetAttributeValue
-            ))
+            Err(Error::Pkcs11(_, Function::SetAttributeValue))
         ));
     }
 
@@ -3903,10 +3900,7 @@ fn validation() -> TestResult {
     } else {
         assert!(matches!(
             res,
-            Err(Error::Pkcs11(
-                RvError::ActionProhibited,
-                Function::SetAttributeValue
-            ))
+            Err(Error::Pkcs11(_, Function::SetAttributeValue))
         ));
     }
 
