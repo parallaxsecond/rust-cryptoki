710794549b1150ee7ae84d260b87ee5ea5b2f4d1
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2025-10-23 11:29:32 +0200

    Make `Session` take a reference to `Pkcs11` and remove inner `Arc`
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- cryptoki/src/session/encryption.rs
+++ cryptoki/src/session/encryption.rs
@@ -10,7 +10,7 @@ use crate::session::Session;
 use cryptoki_sys::*;
 use std::convert::TryInto;
 
-impl Session {
+impl Session<'_> {
     /// Single-part encryption operation
     pub fn encrypt(
         &self,
